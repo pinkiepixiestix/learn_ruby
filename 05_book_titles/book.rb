12df9a9bf4883e3d34e090e511057e02124cc16c
pinkiepixiestix <ren0yeah@gmail.com>
2017-12-08 13:24:59 -0600

    fix book file

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,15 @@
 class Book
 # write your code here
+  def title
+    @book
+  end
+
+  def title=(title)
+    @book = make_title(title)
+  end
+
+  def make_title(title)
+    stop_words = ["and", "in", "on", "of", "the", "a", "an"]
+    title.capitalize.split.map{|word| stop_words.include?(word) && word != 0? word : word.capitalize}.join(" ")
+  end
 end
