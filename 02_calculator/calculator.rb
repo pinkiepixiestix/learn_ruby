907da19421eef226c3867c8279d10b4b454a6350
pinkiepixiestix <ren0yeah@gmail.com>
2017-12-07 18:19:10 -0600

    fix calculator file

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,15 @@
 #write your code here
+def add(augend, addend)
+  augend + addend
+end
+
+def subtract(minuend, subtrahend)
+  minuend - subtrahend
+end
+
+def sum(arr)
+  sum = 0
+  sum = 0 if arr.empty?
+  arr.each { |i| sum += i }
+  return sum
+end
