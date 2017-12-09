f26f2520e4af86cf978332d7ab2ec46211202d8c
pinkiepixiestix <ren0yeah@gmail.com>
2017-12-08 19:06:03 -0600

    fix timer file

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,44 @@
 class Timer
   #write your code here
+  attr_accessor :seconds
+
+  def initialize
+    @seconds = 0
+  end
+
+def time_string
+
+    min = @seconds / 60
+    min = min < 60 ? min : min % 60
+    hour = @seconds / 3600
+    self.seconds %= 60
+    "%02d:%02d:%02d" % [hour,min,seconds]
+  end
+
+  def timer
+    @timer
+  end
 end
+
+#puts Timer.new.seconds=6
+#puts Timer.new.time_string
+
+=begin
+class Timer
+  def seconds
+    @timer = 0
+  end
+
+  def seconds=(secs)
+    @timer = secs
+  end
+
+  def time_string
+
+  end
+
+  def padded
+    @timer/10 < 1 ? '0' + @timer.to_s : @timer.to_s
+  end
+end
+=end
